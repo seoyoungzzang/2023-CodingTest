1309fc1e5313a979bae00472ca8c0a66814a1442
seoyoungzzang <128670559+seoyoungzzang@users.noreply.github.com>
2023-04-27 12:32:46 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);	
+
+		int A = scanner.nextInt(); int B = scanner.nextInt();
+		
+		System.out.println(A+B);
+    }
+}
