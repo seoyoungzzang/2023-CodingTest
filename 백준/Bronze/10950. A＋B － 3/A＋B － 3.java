b31f76b22175f39f34a62a654f9706ee4037bcc1
seoyoungzzang <128670559+seoyoungzzang@users.noreply.github.com>
2023-05-11 10:14:27 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		for (int i = 0; i < n; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+
+			System.out.println(a + b);
+		}
+		
+
+	}
+}
